770b93a969b8c23ca7bfa4a7a358bf2ddc9eaad5
Saurabh Srivastava <140393806+srivastavask@users.noreply.github.com>
2026-08-27 16:24:34 +0530

    Delete Website directory

--- Website/js/script.js
+++
@@ -1,8 +0,0 @@
-document.querySelector('.year').textContent = new Date().getFullYear();
-
-const toggle = document.querySelector('.menu-toggle');
-const links = document.querySelector('.nav-links');
-
-if (toggle) {
-  toggle.addEventListener('click', () => links.classList.toggle('open'));
-}
